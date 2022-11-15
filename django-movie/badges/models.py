f63bb622346131b36fa6b30d5374a6be21b4d771
gobeul <gotommao@gmail.com>
2022-11-15 23:26:56 +0900

    1115 장고 모델 작성- 보완 필요

--- django-movie/badges/models.py
+++ django-movie/badges/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class BadgeList(models.Model):
+    name = models.TextField()
+    before_get = models.TextField() # 획득 전 문구
+    after_get = models.TextField() # 획득 후 문구
+    total_cnt = models.IntegerField()
+
+    
+
+class Badge(models.Model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    # 획득 여부를 넣을 필요가 있나? => DB 생성된 시점에서 이미 획득아녀?
+    represent = models.IntegerField()
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    badgelist = models.ForeignKey(BadgeList, on_delete=models.CASCADE)
--- django-movie/movies/models.py
+++ django-movie/movies/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+# 영화
+class Movie(models.Model):
+    # TMDB latest
+    movieId = models.IntegerField() # id
+    title = models.CharField(max_length=100)
+    release_date = models.TextField()
+    runtime = models.IntegerField()
+    overview = models.TextField()
+    popularity = models.IntegerField()
+    poster_path = models.TextField()
+    ## 장르가 dictionary 형태로 들어옴
+    genre_id = models.IntegerField() #
+    genre_name = models.TextField() #
+
+
+
+    # TMDB credits '/movie/{movie_id}/credits'
+    actors = models.TextField() # 리스트형태 -> 제이슨
+    # director = 모든 제작진이 조회가 됨
+
+
+    # TMDB Videos '/movie/{movie_id}/videos'
+    youtubeKey = models.TextField() #
+
+    # TMDB Image '/movie/{movie_id}/images'
+    backgroundPost = models.TextField() # 제이
+    #  가격????
+
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movie')
+
+
+# 리뷰
+class Review(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])  # 별점
+    isSpoiler = models.BooleanField(default=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_review')
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=50)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # 대댓글
+
+
+class Ticketing(models.Model):
+    date = models.TextField()
+    region = models.TextField()
+    number_of_people = models.IntegerField()
+    seat = models.TextField()
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
+
