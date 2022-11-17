eefc715691699e58eed2e3cc3e73a4f3cf25178c
SuhyungK <dook4966@gmail.com>
2022-11-17 09:39:02 +0900

    json 파일 추ㄱ; ㅏ

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view  
+from .serializers import MovieSerializer
+# import requests
 
 # Create your views here.
+@api_view(['GET'])
+def index(request):
+    movies_serializers = MovieSerializer(Movie.objects.all(), many=True)
+    return Response(movies_serializers.data)
