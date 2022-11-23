5e12f51224a133b3dea2385bb77d6ee740d8ab72
SuhyungK <dook4966@gmail.com>
2022-11-24 08:56:43 +0900

    1124

--- final-pjt-front/src/components/NowPlayingMovie.vue
+++ final-pjt-front/src/components/NowPlayingMovie.vue
@@ -7,14 +7,28 @@
 
     <!-- Now Playing Movie -->
     <div class="d-flex mb-4 position-relative" style="overflow-x: scroll">
-      <div v-if="showInfo" class="bg-dark rounded" id="image-mouse-hover"></div>
-      <img @mouserover="showInfo=true" v-for="(npMovie, i) in nowPlayingMovieList" :key="i" 
-        :src="`https://image.tmdb.org/t/p/w500/` + npMovie.poster_path" 
-        :alt="npMovie.title"
-        @mouseover="showInfo" 
-        style="width: 200px; cursor: pointer;"
-        class="me-3 rounded"
+      <div v-for="(npMovie, i) in nowPlayingMovieList" :key="i"
+        class="now-play"
       >
+        <div class="position-absolute d-flex flex-column justify-content-center" style="width: 100px;">
+          <button class="btn btn-success position absolute" id="button-mouse-hover" value="test">
+            ??
+          </button>
+          <button class="btn btn-warning position absolute" id="button-mouse-hover" value="test">
+            ??
+          </button>
+        </div>
+        <img 
+          :src="`https://image.tmdb.org/t/p/w500/` + npMovie.poster_path" 
+          :alt="npMovie.title"
+          @mouseover="showInfo" 
+          style="width: 200px; cursor: pointer;"
+          class="me-3 rounded"
+          @click="test(npMovie)"
+        >
+
+      </div>
+      <div v-if="showInfo" class="bg-dark rounded" id="image-mouse-hover"></div>
     </div>
 
    <hr>
@@ -69,5 +83,9 @@ export default {
   z-index: 10000;
   position: absolute;
   opacity: 0.5;
+}
+
+.now-play > img:hover {
+  opacity: 0.7;
 }
 </style>
